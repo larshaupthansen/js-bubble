bfeb4c03d5ff3b39a6dff37b10dd438af1ab7bcd
Lars Haupt-Hansen <lars@haupt-hansen.dk>
2017-03-01 18:20:29 +0100

    Initial styling and functionality

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,6 +5,33 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
-  title = 'app works!';
+  alpha = 0;
+  beta =0;
+  gamma = 0;
+
+  public constructor() {
+
+
+//     if ((<any>window).DeviceOrientationEvent) {
+//       window.addEventListener('deviceorientation', this.deviceOrientationHandler, false);
+//     }
+  }
+
+  deviceOrientationHandler(e: any): boolean {
+
+    this.alpha = e.alpha;
+    this.beta = e.beta;
+    this.gamma = e.gamma;
+   
+    var bubble = document.getElementById("bubble");
+
+    var left = -50 + this.gamma;
+    var top = -50 + this.beta;
+    bubble.style.marginLeft = left.toString()+"px";
+    bubble.style.marginTop = top.toString()+"px";
+
+    return true;
+  }
 }
